a1335ee3b447bd1c1a2b0ea7a5a974ddc22ca92a
daanvanrobays <vanrobays.daan@hotmail.com>
2019-03-03 22:42:50 +0100

    way with words & refactor website

--- MVPSolutions/Controllers/StoriesController.cs
+++ MVPSolutions/Controllers/StoriesController.cs
@@ -32,5 +32,10 @@ namespace MVPSolutions.Controllers
         {
             return View();
         }
+
+        public ActionResult WayWithWords()
+        {
+            return View();
+        }
     }
 }
